7177fd9a779946d39c83a42040909278aef8b843
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2025-02-03 13:33:59 +0200

    improve validation

--- src/movie/schemas.js
+++ src/movie/schemas.js
@@ -2,15 +2,15 @@ const { boolean, number, object, string } = require('yup');
 
 
 const create = object({
-    name: string().required(),
-    genre: string().required(),
-    rating: number().required().min(1).max(10),
+    name: string().trim().min(2).required(),
+    genre: string().trim().min(2).required(),
+    rating: number().min(1).max(10).required(),
     explicit: boolean().required()
 })
 
 const update = object({
-    name: string(),
-    genre: string(),
+    name: string().trim().min(2),
+    genre: string().trim().min(2),
     rating: number().min(1).max(10),
     explicit: boolean()
 })
